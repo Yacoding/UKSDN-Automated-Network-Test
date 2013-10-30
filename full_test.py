f3d9cb6bcbd436aafdd934d0a673bc448393b0c3
David Pennington <dpenning1@gmail.com>
2013-10-30 13:30:40 -0400

    fixed into semi final form

--- full_test.py
+++ full_test.py
@@ -1,33 +1,11 @@
 import test_distribution as td
 
-testing = True
-
-list_of_connections = []
-
-if testing:
-	s = """[0, 1, 1, 1, 1, 1, 1, 1]
-	[0, 0, 1, 1, 1, 1, 1, 1]
-	[0, 0, 0, 1, 1, 1, 1, 1]
-	[0, 0, 0, 0, 1, 1, 1, 1]
-	[0, 0, 0, 0, 0, 1, 1, 1]
-	[0, 0, 0, 0, 0, 0, 1, 1]
-	[0, 0, 0, 0, 0, 0, 0, 1]
-	[0, 0, 0, 0, 0, 0, 0, 0]
-	""".replace(",","").replace(" ","").replace(
-	"[","").replace("]","").replace("\t","")
-	print s
+def find_exclusive_networks():
 	list_of_connections = []
-	for a in s.split():
-		list_of_connections.append([int(b) for b in a])
-	for a in list_of_connections:
-		print a
-	hosts = td.list_of_hosts
-else:
 	hosts = td.list_of_hosts
 	for a in xrange(len(hosts)):
 		l = [] + [0]*a
 		for b in xrange(a,len(hosts)):
-			print a,b
 			if a != b:
 				x = td.test_connection(hosts[a],hosts[b])
 				if x[0]:
@@ -39,22 +17,21 @@ else:
 		list_of_connections.append(l)
 
 
-grouped = [0]*len(hosts)
-groups = []
-for a in xrange(len(hosts)):
-	current_grouping = []
-	if grouped[a] != 0:
-		grouped[a] = 1
-		current_grouping.append(a)
-		index = 0
-		while index < len(current_grouping):
-			for b in xrange(len(hosts)):
-				if grouped[b] == 0 and list_of_connections[a][b] == 1:
-					grouped[b] = 1
+	grouped = [0]*len(hosts)
+	groups = []
+	for a in xrange(len(hosts)):
+		print "g ",grouped
+		current_grouping = []
+		if grouped[a] == 0:
+			grouped[a] = 1
+			current_grouping.append(a)
+			for b in xrange(len(list_of_connections[a])):
+				if list_of_connections[a][b] == 1 and grouped[b] == 0:
 					current_grouping.append(b)
-			index += 1
-	if current_grouping != []:
-		groups.append(current_grouping)
-print groups
-for a in groups:
-	print a
+					grouped[b] = 1
+		if current_grouping != []:
+			groups.append(current_grouping)
+	return groups
+
+if __name__ == "__main__":
+	print find_exclusive_networks()
