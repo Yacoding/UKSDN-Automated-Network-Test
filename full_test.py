533d4ff94a113a01cc26126165d6986eb89f63d8
David Pennington <dpenning1@gmail.com>
2013-11-01 12:12:39 -0400

    Update full_test.py

--- full_test.py
+++ full_test.py
@@ -1,3 +1,4 @@
+import time
 import test_distribution as td
 
 def find_exclusive_networks():
@@ -23,17 +24,19 @@ def find_exclusive_networks():
 		current_grouping = []
 		if grouped[a] == 0:
 			grouped[a] = 1
-			current_grouping.append(a)
+			current_grouping.append(hosts[a])
 			for b in xrange(len(list_of_connections[a])):
 				if list_of_connections[a][b] == 1 and grouped[b] == 0:
-					current_grouping.append(b)
+					current_grouping.append(hosts[b])
 					grouped[b] = 1
 		if current_grouping != []:
 			groups.append(current_grouping)
 	return groups
 
 if __name__ == "__main__":
-	count = 1	
-	for a in find_exclusive_networks():
-		print "Group " + str(count) + "\t" + str(a)
-		count += 1
+	while True:
+		print (time.strftime("%H:%M:%S"))
+		count = 1	
+		for a in find_exclusive_networks():
+			print "Group " + str(count) + "\t" + str(a)
+			count += 1
