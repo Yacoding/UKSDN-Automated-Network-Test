8c5b3b13b5f45632d96c1a842a6216975229c861
David Pennington <dpenning1@gmail.com>
2013-11-14 13:17:25 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,17 +1,51 @@
 import tornado.ioloop
 import tornado.web
+import t2h
 
-class MainHandler(tornado.web.RequestHandler):
+class GroupHandler(tornado.web.RequestHandler):
     def get(self):
-	f = open("test_log.txt",'r')
+	f = open("test_log_groups.txt",'r')
 	x = '<br>'.join(f.readlines())
 	f.close()
         self.write(x)
+class ConnectionsHandler(tornado.web.RequestHandler):
+    def get(self):
+        f = open("test_log_connections.txt",'r')
+        x = '<br>'.join(f.readlines())
+        f.close()
+        self.write(x)
+class SpeedHandler(tornado.web.RequestHandler):
+    def get(self):
+        f = open("test_log_speeds.txt",'r')
+        x = '<br>'.join(f.readlines())
+        f.close()
+        self.write(x)
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        f = open("test_log_groups.txt",'r')
+        x = '<br>' + '<br>'.join(f.readlines())
+        f.close()
+	f = open("test_log_connections.txt",'r')
+        x += '<br>' + '<br>'.join(f.readlines())
+        f.close()
+	f = open("test_log_speeds.txt",'r')
+        x += '<br>' + '<br>'.join(f.readlines())
+        f.close()
+        self.write(x)
+class SingleConnectionHandler(tornado.web.RequestHandler):
+	def get(self,host_1,host_2):
+		x = t2h.web_test(host_1,host_2)
+		self.write(x[1])	
+
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/groups", GroupHandler),
+    (r"/connections", ConnectionsHandler),
+    (r"/speeds", SpeedHandler),
+    (r"/(.+)-(.+)", SingleConnectionHandler),
+    (r"/.*", MainHandler),
 ])
 
 if __name__ == "__main__":
-    application.listen(6666)
+    application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
